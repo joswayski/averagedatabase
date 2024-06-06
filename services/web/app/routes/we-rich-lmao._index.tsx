8bd34e602b321c1d534339aa9817991e6d5deccf
Jose Valerio <contact@josevalerio.com>
2024-06-06 04:23:08 -0400

    stuff

--- services/web/app/routes/we-rich-lmao._index.tsx
+++ services/web/app/routes/we-rich-lmao._index.tsx
@@ -81,23 +81,17 @@ export default function WeRich() {
               </span>
             </li>
           </ul>
+
           <p className="mt-8">
-            I hope this makes it clear what our roadmap is going forward. To all
-            of our users, thank you for being part of this journey with us. To
-            our enterprise customers, thank fucking god you&apos;re here.
+            I hope this makes it clear what our roadmap is going forward. These
+            last few years haven&apos;t been the greatest on the industry as a
+            whole. We almost had to do a license rugpull to stay afloat!
+            Luckily, due to our amazing and wonderful ENTERPRISE CUSTOMERS (you
+            know who you are!!), things are looking great going forward.
+            I&apos;ll leave you with some internal numbers our data science team
+            (Jake (Stanford (Dropout))) put together using Databricks ($43k/mo):
           </p>
-          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-            From beginner to expert in 3 hours
-          </h2>
-          <p className="mt-6">
-            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-            diam.
-          </p>
-          <figure className="mt-10 border-l border-indigo-600 pl-9">
+          {/* <figure className="mt-10 border-l border-indigo-600 pl-9">
             <blockquote className="font-semibold text-gray-900">
               <p>
                 “Vel ultricies morbi odio facilisi ultrices accumsan donec lacus
@@ -109,7 +103,7 @@ export default function WeRich() {
             <figcaption className="mt-6 flex gap-x-4">
               <img
                 className="h-6 w-6 flex-none rounded-full bg-gray-50"
-                src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                src="/public/ebitda.png"
                 alt=""
               />
               <div className="text-sm leading-6">
@@ -119,47 +113,21 @@ export default function WeRich() {
                 – Marketing Manager
               </div>
             </figcaption>
-          </figure>
-          <p className="mt-10">
+          </figure> */}
+          {/* <p className="mt-10">
             Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
             enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
             praesent donec est. Odio penatibus risus viverra tellus varius sit
             neque erat velit.
-          </p>
+          </p> */}
         </div>
         <figure className="mt-16">
           <img
             className="aspect-video rounded-xl bg-gray-50 object-cover"
-            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
+            src="/public/ebitda.png"
             alt=""
           />
-          <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
-            <InformationCircleIcon
-              className="mt-0.5 h-5 w-5 flex-none text-gray-300"
-              aria-hidden="true"
-            />
-            Faucibus commodo massa rhoncus, volutpat.
-          </figcaption>
         </figure>
-        <div className="mt-16 max-w-2xl">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            Everything you need to get up and running
-          </h2>
-          <p className="mt-6">
-            Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam
-            varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales
-            cursus tristique. Tincidunt sed tempus ut viverra ridiculus non
-            molestie. Gravida quis fringilla amet eget dui tempor dignissim.
-            Facilisis auctor venenatis varius nunc, congue erat ac. Cras
-            fermentum convallis quam.
-          </p>
-          <p className="mt-8">
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-        </div>
       </div>
     </div>
   );
