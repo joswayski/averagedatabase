954419d623a075f9bceccb970e04590a8581443d
Jose Valerio <contact@josevalerio.com>
2024-06-06 02:20:04 -0400

    stuff

--- services/web/app/components/pricing.tsx
+++ services/web/app/components/pricing.tsx
@@ -16,6 +16,7 @@ const tiers = [
       annually: "$0",
     },
     hideFrequency: false,
+    priceSubtext: "IF we have compute leftover we'll serve your requests",
     description:
       "Just enough to put a side project on your resume before your next FAANG gig",
     features: [
@@ -34,9 +35,10 @@ const tiers = [
     ),
     id: "tier-startup",
     href: "#",
-    price: { monthly: "$39", annually: "$279" },
+    price: { monthly: "$39", annually: "$299" },
     description:
       "You're trying to get into YC and haven't setup proper accounting yet",
+    priceSubtext: "1/256th vCPU & 4mb RAM. Don't use it all up now!",
     features: [
       "4kb payloads",
       "Dashboard to login & view your account info",
@@ -202,6 +204,23 @@ export const Pricing = () => {
             </div>
           ))}
         </div>
+        {/* <div className="flex flex-col items-start gap-x-8 gap-y-6 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-2 lg:flex-row lg:items-center">
+          <div className="lg:min-w-0 lg:flex-1">
+            <h3 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
+              Discounted
+            </h3>
+            <p className="mt-1 text-base leading-7 text-gray-600">
+              Dolor dolores repudiandae doloribus. Rerum sunt aut eum. Odit
+              omnis non voluptatem sunt eos nostrum.
+            </p>
+          </div>
+          <a
+            href="https://github.com/joswayski/averagedatabase"
+            className="rounded-md px-3.5 py-2 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Buy discounted license <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div> */}
       </div>
     </div>
   );
