1904a5abe0bb96dca37da0baf510792742326209
Jose Valerio <contact@josevalerio.com>
2024-06-06 01:40:13 -0400

    stuff

--- services/web/app/routes/uptime._index.tsx
+++ services/web/app/routes/uptime._index.tsx
@@ -1,22 +1,16 @@
-import { getPercentageOf } from "~/utils/getPct";
-import { getRandomNumber } from "~/utils/randomNumber";
-
-import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/20/solid";
-
-const;
 const legitData = [
   {
-    name: "Uptime 30d",
+    name: "30d",
     stat: "100%",
     changeType: "same",
   },
   {
-    name: "Uptime 90d",
+    name: "90d",
     stat: "100%",
     changeType: "same",
   },
   {
-    name: "Uptime All Time",
+    name: "All Time",
     stat: "100%",
     changeType: "same",
   },
@@ -33,38 +27,53 @@ const stats = [
     title: "Backups",
   },
   {
+    title: "Dashboard",
+  },
+  {
     title: "Team Morale",
   },
 ];
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Uptime() {
   return (
-    <div className="flex flex-col space-y-6 p-12">
-      {stats.map((stat) => (
-        <div key={stat.title}>
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            {stat.title}
-          </h3>
-          <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0">
-            {legitData.map((item) => (
-              <div key={item.name} className="px-4 py-5 sm:p-6">
-                <dt className="text-base font-normal text-gray-900">
-                  {item.name}
-                </dt>
-                <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
-                  <div className="flex items-baseline text-2xl font-semibold text-emerald-600">
-                    {item.stat}
-                  </div>
-                </dd>
-              </div>
-            ))}
-          </dl>
+    <div className="flex flex-col space-y-6 p-12 border border-blue-500 w-full max-w-4xl ">
+      <div className=" border border-indigo-700 rounded-md">
+        <div className="bg-indigo-500 rounded-t-md p-4 text-white">
+          <h1 className="text-3xl font-bold text-white">
+            We are fully operational!
+          </h1>
         </div>
-      ))}
+        <div className="p-4">
+          <p className="text-slate-700">
+            We are not aware of any issues affecting our systems. Ping us on
+            Twitter if something goes down.
+          </p>
+        </div>
+      </div>
+
+      <div>
+        {stats.map((stat) => (
+          <div key={stat.title}>
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              {stat.title}
+            </h3>
+            <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0">
+              {legitData.map((item) => (
+                <div key={item.name} className="px-4 py-5 sm:p-6">
+                  <dt className="text-base font-normal text-gray-900">
+                    {item.name}
+                  </dt>
+                  <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
+                    <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
+                      {item.stat}
+                    </div>
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- services/web/app/routes/uptime.tsx
+++ services/web/app/routes/uptime.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@remix-run/react";
 
 export default function UptimeLayout() {
   return (
-    <div className="bg-white  mt-34 border border-red-400">
+    <div className="bg-white   flex justify-center border border-red-400 ">
       <Outlet />;
     </div>
   );
