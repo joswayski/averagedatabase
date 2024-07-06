6c909992abbb95f2a30b1043416d4ba0819f9426
Jose Valerio <contact@josevalerio.com>
2024-07-05 22:30:42 -0400

    foter

--- services/web/app/components/footer.tsx
+++ services/web/app/components/footer.tsx
@@ -26,8 +26,8 @@ const navigation = [
 export default function Footer() {
   return (
     <footer className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center space-x-6 md:order-2">
+      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-evenly lg:space-x-20 lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2 ">
           {navigation.map((item) => (
             <a
               key={item.name}
@@ -41,7 +41,7 @@ export default function Footer() {
             </a>
           ))}
         </div>
-        <div className="mt-8 md:order-1 md:mt-0 flex justify-center">
+        <div className="mt-8 md:order-1 md:mt-0 flex justify-center ">
           <p className="text-center text-xs leading-5 text-gray-500">
             &copy; 2024 Average Labs LLC /{" "}
             <a
