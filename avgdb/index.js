0ba54013eac9932b269bbc09efeabf1f56628435
Jose Valerio <contact@josevalerio.com>
2025-06-09 18:47:19 -0400

    saving

--- avgdb/index.js
+++ avgdb/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const kleur = require("kleur");
+const figlet = require("figlet");
+
 const getApiKey = async () => {
   try {
     const response = await fetch("https://api.averagedatabase.com/gibs-key", {
@@ -14,24 +17,34 @@ const getApiKey = async () => {
           errorData.message || "The API is probably down, sorry bud."
         }`;
       } catch (e) {}
-      console.error(errorMessage);
+      console.error(kleur.red(errorMessage));
       process.exit(1);
     }
 
     const { api_key, brought_to_you_by } = await response.json();
-    console.log(`\nYour API key is: ${api_key}`);
-    console.log("View the docs at https://averagedatabase.com/docs");
+
+    console.log(
+      kleur.blue(figlet.textSync("AvgDB", { horizontalLayout: "full" }))
+    );
+    console.log(`\nYour API key is: ${kleur.bold().yellow(api_key)}`);
+    console.log(
+      `View the docs at ${kleur.cyan("https://averagedatabase.com/docs")}`
+    );
     if (brought_to_you_by) {
-      console.log(`\nBrought to you by: ${brought_to_you_by}`);
+      console.log(kleur.dim(`\nBrought to you by: ${brought_to_you_by}`));
     }
     console.log(
-      "\nKeep this key safe and use it in the 'x-averagedb-api-key' header for authenticated requests."
+      kleur.green(
+        "\nKeep this key safe and use it in the 'x-averagedb-api-key' header for authenticated requests."
+      )
     );
   } catch (error) {
     console.error(
-      "Error: Could not connect to the server. Check your internet connection or our server status."
+      kleur.red(
+        "Error: Could not connect to the server. Check your internet connection or our server status."
+      )
     );
-    console.error(error.message);
+    console.error(kleur.red(error.message));
     process.exit(1);
   }
 };
